dea4cac2d234392f656aeffaaff76d9be73642b8
Adam Niemczura <145414172+AdamNiem@users.noreply.github.com>
2026-05-27 16:32:09 -0400

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        cache = [0 for _ in nums]
+        def dfs(i):
+            # from 0 we can jump +1 or +2 n times to get to any house i
+            if i >= len(nums):
+                return 0
+            
+            
+            
+            if cache[i] != 0:
+                return cache[i]
+            
+            cache[i] = max(
+                nums[i] + dfs(i + 2), 
+                dfs(i + 1)
+            )
+
+            return cache[i]
+            
+        dfs(0)
+        return dfs(0)
